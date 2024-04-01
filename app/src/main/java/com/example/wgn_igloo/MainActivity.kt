10ad0fe9f9b8dde23069f236e332da1f278054c4
Nicole Tran <nktran3@bu.edu>
2024-04-01 03:09:53 -0400

    set up fragments and bottom nav bar

--- app/src/main/java/com/example/wgn_igloo/MainActivity.kt
+++ app/src/main/java/com/example/wgn_igloo/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.wgn_igloo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var selectedFragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId= R.id.home_nav
+
+        bottomNavigation.setOnItemSelectedListener{
+            if (it.itemId == R.id.recipe_nav) {
+                selectedFragment = RecipesPage()
+            } else if (it.itemId == R.id.shopping_list_nav) {
+                selectedFragment = ShoppingListPage()
+            } else if (it.itemId == R.id.home_nav) {
+                selectedFragment = HomePage()
+            } else if (it.itemId == R.id.inbox_nav) {
+                selectedFragment = InboxPage()
+            } else if (it.itemId == R.id.profile_nav) {
+                selectedFragment = ProfilePage()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+
+            true
+        }
+
+
+
+
     }
 }
