035e97826716d260b84443999309eb589f21976d
Gary Dong <garyd89@bu.edu>
2024-04-02 17:43:47 -0400

    integrated auth email/password into layout

--- app/src/main/java/com/example/wgn_igloo/ProfilePage.kt
+++ app/src/main/java/com/example/wgn_igloo/ProfilePage.kt
@@ -1,15 +1,23 @@
 package com.example.wgn_igloo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wgn_igloo.databinding.FragmentProfilePageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private const val TAG = "ProfilePage"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,12 +29,20 @@ class ProfilePage : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentProfilePageBinding
+
+    private lateinit var auth: FirebaseAuth
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -34,8 +50,16 @@ class ProfilePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_page, container, false)
+        binding = FragmentProfilePageBinding.inflate(inflater, container, false)
+        binding.logoutButton.setOnClickListener(){
+            Firebase.auth.signOut()
+            goToLoginActivity()
+            Log.d(TAG, "Signed out")
+        }
+
+        return binding.root
     }
+
 
     companion object {
         /**
@@ -56,4 +80,12 @@ class ProfilePage : Fragment() {
                 }
             }
     }
+
+    fun goToLoginActivity() {
+        val intent = Intent(activity, LoginActivity::class.java)
+        // You can also put extra data to pass to the destination activity
+        intent.putExtra("key", "value")
+        startActivity(intent)
+    }
+
 }
