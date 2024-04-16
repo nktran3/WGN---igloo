6f913bc70dc73424132220a69fee076e69234125
Nicole Tran <nktran3@bu.edu>
2024-04-16 18:59:35 -0400

    adding members and faq pages

--- app/src/main/java/com/example/wgn_igloo/ProfilePage.kt
+++ app/src/main/java/com/example/wgn_igloo/ProfilePage.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.constraintlayout.helper.widget.Carousel
 import androidx.fragment.app.Fragment
 import com.example.wgn_igloo.databinding.FragmentProfilePageBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -22,8 +26,6 @@ class ProfilePage : Fragment() {
 
     private lateinit var auth: FirebaseAuth
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize Firebase Auth
@@ -36,13 +38,40 @@ class ProfilePage : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentProfilePageBinding.inflate(inflater, container, false)
-        binding.logoutButton.setOnClickListener(){
-            Firebase.auth.signOut()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileButton.setOnClickListener {
+            Toast.makeText(context, "Profile clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.settingsButton.setOnClickListener {
+            Toast.makeText(context, "Settings clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.membersButton.setOnClickListener {
+            val membersFragment = Members()
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, membersFragment).commit()
+        }
+
+        binding.faqButton.setOnClickListener {
+            val faqFragment = FaqFragment()
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, faqFragment).commit()
+        }
+
+//        binding.supportButton.setOnClickListener {
+//            Toast.makeText(context, "Support clicked!", Toast.LENGTH_SHORT).show()
+//        }
+
+        binding.logoutButton.setOnClickListener {
+            //signOut()
             goToLoginActivity()
             Log.d(TAG, "Signed out")
         }
-
-        return binding.root
     }
 
     fun goToLoginActivity() {
