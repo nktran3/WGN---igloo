dd17a06a3a6c4075f14fcf7e25e7690b18eff7f9
Gary Dong <garyd89@bu.edu>
2024-05-02 12:57:13 -0400

    made saved icon stay for saved recipes

--- app/src/main/java/com/example/wgn_igloo/recipe/SavedRecipe.kt
+++ app/src/main/java/com/example/wgn_igloo/recipe/SavedRecipe.kt
@@ -1,9 +1,13 @@
 package com.example.wgn_igloo.recipe
 data class SavedRecipe (
-    val imageId: Int,
-    val recipeName: String,
-    val preparationTime: String,
-    val cookTime: String,
-    val totalTime: String,
-    val servingSize: String
+    var imageId: String = "",
+    var recipeName: String = "",
+    var dishType: String = "",
+    var cuisineType: String = "",
+    var dietType: String = "",
+    var totalTime: String = "",
+    var servingSize: String = "",
+    var instructions: List<String> = listOf(),
+    var ingredients: List<String>? = listOf()
+
 )
