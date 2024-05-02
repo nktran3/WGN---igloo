4a39d387ac021695d28ee7fe04391ccf84e4c1ce
Nicole Tran <nktran3@bu.edu>
2024-05-01 20:24:55 -0400

    redesigned profile page

--- app/src/main/java/com/example/wgn_igloo/profile/ProfilePage.kt
+++ app/src/main/java/com/example/wgn_igloo/profile/ProfilePage.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wgn_igloo.auth.LoginActivity
 import com.example.wgn_igloo.database.Members
 import com.example.wgn_igloo.R
@@ -19,15 +21,29 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.example.wgn_igloo.profile.ProfileItemAdapter.ProfileItem
+import com.google.firebase.firestore.ktx.firestore
 
 private const val TAG = "ProfilePage"
 
 class ProfilePage : Fragment() {
 
-
+    private lateinit var profileAdapter: ProfileItemAdapter
     private lateinit var binding: FragmentProfilePageBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+
+    private val myDataset: List<ProfileItem> = listOf(
+        ProfileItem(R.drawable.profile_icon, "Profile"),
+        ProfileItem(R.drawable.friends_icon, "Friends"),
+        ProfileItem(R.drawable.manage_account_icon, "Manage Account"),
+        ProfileItem(R.drawable.support_icon, "Support"),
+        ProfileItem(R.drawable.faq_icon, "FAQ"),
+        ProfileItem(R.drawable.setting_icon, "Settings"),
+        ProfileItem(R.drawable.about_icon, "About"),
+        ProfileItem(R.drawable.logout_icon, "Logout")
+
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,76 +72,89 @@ class ProfilePage : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.profileButton.setOnClickListener {
-            Toast.makeText(context, "Profile clicked!", Toast.LENGTH_SHORT).show()
-            val profileFragment = ProfileFragment()
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, profileFragment)
-                .addToBackStack(null)
-                .commit()
+        val recyclerView: RecyclerView = view.findViewById(R.id.profile_recycler)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        profileAdapter = ProfileItemAdapter(myDataset) { item ->
+            when (item.text) {
+                "Profile" -> showProfileDetails()
+
+                "Friends" -> showFriends()
+
+                "Manage Account" -> showManageAccount()
+
+                "Support" -> openGoogleApp()
+
+                "FAQ" -> showFAQpage()
+
+                "Settings" -> showSettings()
+
+                "About" -> showAbout()
+
+                "Logout" -> {
+                    signOut()
+                    goToLoginActivity()
+                    Log.d(TAG, "Signed out")
+                }
+
+
+            }
         }
+        recyclerView.adapter = profileAdapter
+        fetchUsername()
 
-        binding.settingsButton.setOnClickListener {
-            Toast.makeText(context, "Settings clicked!", Toast.LENGTH_SHORT).show()
-        }
+    }
+    private fun fetchUsername() {
+        val userId = auth.currentUser?.uid
+        Firebase.firestore.collection("users").document(userId ?: "")
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val username = document.getString("username")
+                    updateUsernameOnUI(username)
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG, "Failed to fetch username", e)
+            }
+    }
 
-        binding.membersButton.setOnClickListener {
-            val membersFragment = Members()
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, membersFragment).commit()
-        }
-
-        binding.faqButton.setOnClickListener {
-            val faqFragment = FaqFragment()
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, faqFragment).commit()
-        }
-
-        // Commented out section for supportButton, uncomment if needed
-        // binding.supportButton.setOnClickListener {
-        //     Toast.makeText(context, "Support clicked!", Toast.LENGTH_SHORT).show()
-        // }
-
-        binding.logoutButton.setOnClickListener {
-            signOut()
-            goToLoginActivity()
-            Log.d(TAG, "Signed out")
-        }
-
-        binding.supportButton.setOnClickListener {
-            openGoogleApp()
+    private fun updateUsernameOnUI(username: String?) {
+        activity?.runOnUiThread {
+            binding.accountName.text = username ?: "Unknown User"
         }
     }
 
-
-    fun goToLoginActivity() {
-        val intent = Intent(activity, LoginActivity::class.java)
-        // You can also put extra data to pass to the destination activity
-        intent.putExtra("key", "value")
-        startActivity(intent)
+    private fun showFriends() {
+        val membersFragment = Members()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, membersFragment).commit()
     }
 
-    private fun signOut() {
-        // Firebase sign out
-        FirebaseAuth.getInstance().signOut()
-
-        // Google sign out
-        googleSignInClient.signOut().addOnCompleteListener {
-            // Handle sign out result
-        }
+    private fun showManageAccount(){
+        Toast.makeText(context, "Manage Account clicked!", Toast.LENGTH_SHORT).show()
+    }
+    private fun showSettings(){
+        Toast.makeText(context, "Settings clicked!", Toast.LENGTH_SHORT).show()
     }
 
-//    private fun openGoogleApp() {
-//        try {
-//            val gmmIntentUri = Uri.parse("googlechrome://navigate?url=https://www.google.com/")
-//            val intent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-//            intent.setPackage("com.google.android.apps.chrome")
-//            startActivity(intent)
-//        } catch (e: Exception) {
-//            val gmmIntentUri = Uri.parse("https://www.google.com/")
-//            val intent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-//            startActivity(intent)
-//        }
-//    }
+    private fun showAbout(){
+        Toast.makeText(context, "Abouts clicked!", Toast.LENGTH_SHORT).show()
+    }
+    private fun showFAQpage(){
+        val faqFragment = FaqFragment()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, faqFragment).commit()
 
+    }
+
+    private fun showProfileDetails(){
+        val profileFragment = ProfileFragment()
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, profileFragment)
+            .addToBackStack(null)
+            .commit()
+
+    }
     private fun openGoogleApp() {
         try {
             // URL for the Google Form
@@ -143,5 +172,24 @@ class ProfilePage : Fragment() {
         }
     }
 
+    private fun goToLoginActivity() {
+        val intent = Intent(activity, LoginActivity::class.java)
+        // You can also put extra data to pass to the destination activity
+        intent.putExtra("key", "value")
+        startActivity(intent)
+    }
+    private fun signOut() {
+        // Firebase sign out
+        FirebaseAuth.getInstance().signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener {
+            // Handle sign out result
+        }
+    }
+
+
 
 }
+
+
