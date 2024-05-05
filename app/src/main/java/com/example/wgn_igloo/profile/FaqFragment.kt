0d5e6f992868af49cf1dd1f5aa2668df666a64c1
Nicole Tran <nktran3@bu.edu>
2024-05-04 22:34:23 -0400

    added questions to faq page

--- app/src/main/java/com/example/wgn_igloo/profile/FaqFragment.kt
+++ app/src/main/java/com/example/wgn_igloo/profile/FaqFragment.kt
@@ -14,9 +14,16 @@ import com.example.wgn_igloo.databinding.FragmentFaqBinding
 class FaqFragment : Fragment() {
     private lateinit var faqAdapter: FaqAdapter
     private var faqList: List<FaqItem> = listOf(
-        FaqItem("How to use the app?", "Here's how you can use the app..."),
-        FaqItem("Where to register?", "You can register on the login page..."),
-        FaqItem("How do you add members to your fridge", "You can add members by...")
+        FaqItem("How does igloo help reduce food waste?", "igloo helps reduce food waste by allowing users to keep track of the items in their fridge, including their expiration dates. With features like notifications for upcoming expirations and recipe suggestions based on fridge contents, users can better plan their meals and use up ingredients before they go to waste."),
+        FaqItem("How do you add items to your fridge?", "You can either manually add items to your fridge inventory or you can use the barcode scanning feature to add items to your fridge inventory in igloo."),
+        FaqItem("How do you manage the items in your fridge?", "You can manage items in your fridge by clicking the item you want to manage, when you do this the item's details will show and from their you can either edit, delete, or add your items to your shopping list."),
+        FaqItem("Is the barcode scanner feature available for all items?", "The barcode scanner feature in igloo works for most items with barcodes. However, some unique or specialty items may not be recognized by the scanner."),
+        FaqItem("How does the recipe search feature work?", "The recipe search feature in igloo allows users to input the ingredients they have in their fridge, and the app will generate recipe suggestions based on those ingredients. It allows users to search up recipes that they want to make or save for later. igloo also suggests recipes for users based on what they already have in their fridge, so no extra searching is needed."),
+        FaqItem("Can I share my fridge inventory with others?", "Yes, igloo allows users to share their fridge inventory with friends, family, or roommates. This feature is especially useful for those living in shared spaces to keep track of food ownership and prevent confusion. Users are also able to share items with their friends or request to borrow items from their friend's inventory."),
+        FaqItem("How do notifications work in igloo?", "igloo's notification system keeps users informed about upcoming expiration dates, borrowing requests, and request responses."),
+        FaqItem("How does the shopping list feature work?", "Users are able to add items to their shopping list for when they go grocery shopping. Once the user buys the items in their shopping list, they can easily add it to their inventory by clicking the check box. Users can either add items to their shopping list using the input form or by clicking the add to shopping list button from their inventory."),
+        FaqItem("Is igloo available on both iOS and Android devices?", "Currently, igloo is only available for Android devices, but we are currently working to make it available for iOS devices."),
+        FaqItem("How can I provide feedback or report issues with igloo?", "We welcome feedback from our users! You can provide feedback or report any issues with igloo by clicking the support button available on the accounts page. This will lead you to a form that you can use to send your feedback or report issues. Our team works very hard to respond to feed back in a timely manner."),
     )
     private var _binding: FragmentFaqBinding? = null
     private val binding get() = _binding!!
